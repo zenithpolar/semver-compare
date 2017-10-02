dfdead531cef899d5a5fb6cedafcf111788dbe52
zenithpolar <zenithpolar@users.noreply.github.com>
2017-10-02 15:17:41 -0300

    Parameterized separators and auto length check
    Multiple separators can be passed as an escaped array considering the special regex simbols.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-module.exports = function cmp (a, b) {
-    var pa = a.split('.');
-    var pb = b.split('.');
-    for (var i = 0; i < 3; i++) {
+module.exports = function cmp (a, b, separators=['\\.','-']) {
+    let splitter = new RegExp(separators.join('|'), 'g');
+    let pa = a.split(splitter);
+    let pb = b.split(splitter);
+    for (var i = 0; i < Math.min(pa.length,pb.length); i++) {
         var na = Number(pa[i]);
         var nb = Number(pb[i]);
         if (na > nb) return 1;
         if (nb > na) return -1;
         if (!isNaN(na) && isNaN(nb)) return 1;
         if (isNaN(na) && !isNaN(nb)) return -1;
+    }
+    if(pa.length > pb.length){
+        return 1;
+    }
+    if(pb.length > pa.length){
+        return -1;
     }
     return 0;
 };
