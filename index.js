4e632f9e710201924962187939b168d183d65f62
zenithpolar <zenithpolar@users.noreply.github.com>
2017-10-02 15:38:57 -0300

    Generator of comparer

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-module.exports = function cmp (a, b, separators=['\\.','-']) {
-    let splitter = new RegExp(separators.join('|'), 'g');
-    let pa = a.split(splitter);
-    let pb = b.split(splitter);
-    for (var i = 0; i < Math.min(pa.length,pb.length); i++) {
-        var na = Number(pa[i]);
-        var nb = Number(pb[i]);
-        if (na > nb) return 1;
-        if (nb > na) return -1;
-        if (!isNaN(na) && isNaN(nb)) return 1;
-        if (isNaN(na) && !isNaN(nb)) return -1;
-    }
-    if(pa.length > pb.length){
-        return 1;
-    }
-    if(pb.length > pa.length){
-        return -1;
-    }
-    return 0;
-};
+module.exports = return (separators=['\\.','-']) => {
+    function cmp (a, b) {
+        let splitter = new RegExp(separators.join('|'), 'g');
+        let pa = a.split(splitter);
+        let pb = b.split(splitter);
+        for (var i = 0; i < Math.min(pa.length,pb.length); i++) {
+            var na = Number(pa[i]);
+            var nb = Number(pb[i]);
+            if (na > nb) return 1;
+            if (nb > na) return -1;
+            if (!isNaN(na) && isNaN(nb)) return 1;
+            if (isNaN(na) && !isNaN(nb)) return -1;
+        }
+        if(pa.length > pb.length){
+            return 1;
+        }
+        if(pb.length > pa.length){
+            return -1;
+        }
+        return 0;
+    };
+}
